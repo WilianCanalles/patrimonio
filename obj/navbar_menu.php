75c9d7adb141504d10dbad14cb30f3212b65f18c
WilianCanalles <wilcanalles@gmail.com>
2020-02-27 23:50:07 -0300

    outros obj do site

---
+++ obj/navbar_menu.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+<header class="sticky-top">
+    <nav class="navbar navbar-expand-md navbar-light bg-opacidade">
+      <div class="container">
+        <a href="index.html" class="navbar-brand">
+          <img src="https://source.unsplash.com/random/50x50">
+          <span class="">Nosso Patrimonio</span>
+        </a>
+
+        <!-- Botao menu compactado -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar_menu">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- Fim Botao menu compactado -->
+        <!-- Menu -->
+
+        <div class="collapse navbar-collapse" id="navbar_menu">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a href="" class="nav-link">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a href="" class="nav-link">Sobre</a>
+            </li>
+            <li class="nav-item divisor"></li>
+
+            <li class="nav-item">
+              <a href="" class="nav-link">Inscrever-se</a>
+            </li>
+            <li class="nav-item">
+              <a href="" class="nav-link">Entrar</a>
+            </li>
+          </ul>
+        </div>
+        <!-- Fim Menu -->
+      </div>
+    </nav>
+  </header>
+</body>
+</html>
