1939a5a43bc6c4239d2d555ea5b99d3f39d66783
WilianCanalles <wilcanalles@gmail.com>
2020-02-27 23:39:52 -0300

    Inicio de projeto

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Meta tags Obrigatórias -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- JavaScript (Opcional) -->
+  <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+  <!-- HTML5Shiv -->
+  <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <![endif]-->
+
+  <!-- Estilo customizado -->
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+
+  <title>Nosso Patrimonio</title>
+</head>
+
+<body>
+  <section>
+    <!--Informacoes do site-->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 ">
+          <div id="carousel-menu" class="carousel" data-ride="carousel">              
+            <div class="carousel-inner"><!--Inner -->
+
+              <div class="carousel-item active">
+                <img src="https://source.unsplash.com/WLUHO9A_xik/1110x500">
+              </div>
+              <div class="carousel-item">
+                <img src="https://source.unsplash.com/random/1110x500">
+              </div>
+
+            </div>
+            <a href="#carousel-menu" class="carousel-control-prev" data-slide="prev">
+                <i class="fas fa-angle-left fa-3x"></i>
+              </a>
+
+              <a href="#carousel-menu" class="carousel-control-next" data-slide="next">
+                <i class="fas fa-angle-right fa-3x"></i>
+              </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<!-- barra de menu -->
+  <?php include 'obj/navbar_menu.php' ?>
+<!-- fim barra de menu -->
+
+  <section>
+    <!--Informacoes do site-->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+        </div>
+        <div class="col-md-6">
+          <p>Coluna2</p>
+          <p>Coluna2</p>
+          <p>Coluna2</p>
+          <p>Coluna2</p>
+          <p>Coluna2</p>
+          <p>Coluna2</p>
+          <p>Coluna2</p>
+          <p>Coluna2</p>
+          <p>Coluna2</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna1</p>
+          <p>Coluna2</p>
+          <p>Coluna2</p>
+          <p>Coluna2</p>
+          <p>Coluna2</p>
+          <p>Coluna2</p>
+          <p>Coluna2</p>
+          <p>Coluna2</p>
+          <p>Coluna2</p>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+</body>
+
+</html>
