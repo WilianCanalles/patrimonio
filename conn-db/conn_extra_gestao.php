537b8f96a677a24eb11f2c12c92ee9d1492adb71
Wilian Canalles <wilcanalles@gmail.com>
2020-09-13 19:00:59 -0300

    gestão de Equipamentos

--- conn-db/conn_extra_gestao.php
+++ conn-db/conn_extra_gestao.php
@@ -5,6 +5,8 @@ if (!isset($_SESSION)) {
 $emp_principal = $_SESSION['emp_principal'];
 if (isset($_POST['verifica']) && $_POST['verifica'] == 'extra') {
     $_SESSION['conn_extra'] = true;
+} elseif (isset($_POST['verifica']) && $_POST['verifica'] == 'altera') {
+    $_SESSION['conn_extra1'] = true;
 }
 
 if (isset($_SESSION['conn_extra'])) {
@@ -50,7 +52,7 @@ if (isset($_SESSION['conn_extra'])) {
                         <div>
                             <span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Código</span>
 
-                            <span class="form-control" style="color: black!important; height: auto;"><?php echo $line['extra_cod'] ?></span>
+                            <span id="codigo" class="form-control" style="color: black!important; height: auto;"><?php echo $line['extra_cod'] ?></span>
                         </div>
                         <div>
                             <span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Tipo</span>
@@ -107,6 +109,161 @@ if (isset($_SESSION['conn_extra'])) {
                             <span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Situação</span>
 
                             <span class="form-control" style="color: black!important; height: auto;"><?php echo $line['situacao'] ?></span>
+                        </div>
+                        <div>
+                            <span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Informacões</span>
+
+                            <span class="form-control" style="color: black!important; height: auto;"><?php echo $line['informacoes'] ?></span>
+                        </div>
+                        <div>
+                            <span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Periférico</span>
+
+                            <span class="form-control" style="color: black!important; height: auto;"><?php echo $line['perifericos'] ?></span>
+                        </div>
+                    </section>
+                <?php } ?>
+            </div>
+        </div>
+    <?php
+    } catch (PDOException $e) {
+        echo '<p>' . $e->getMessage() . '</p>';
+    }
+    unset($_SESSION['conn_extra']);
+} elseif (isset($_SESSION['conn_extra1'])) {
+    $codigo = $_POST['cod'];
+    include_once 'conexao.php';
+
+    try {
+        $conexao = new PDO(
+            "mysql:host=$host; dbname=$dbname",
+            "$user",
+            "$pass"
+        );
+        $query_tb_equipamento = "SELECT EE.`extra_cod`, T.`tipo_equipamento`, M.`modelo_equipamento`, F.`fabricante`, `num_serie`, S.`empresa`, L.`local`, FO.`fornecedor`, `nota_fiscal`, `data_compra`, `situacao`, `informacoes`, `perifericos`
+    FROM `tb_equipamento` AS EE 
+    LEFT JOIN (SELECT * FROM `tb_fabricante` WHERE `tb_fabricante`.`emp_Principal` = $emp_principal) AS F ON F.`extra_cod` = EE.`fabricante` 
+    LEFT JOIN (SELECT * FROM `tb_fornecedor` WHERE `tb_fornecedor`.`emp_Principal` = $emp_principal) AS FO ON FO.`extra_cod` = EE.`fornecedor` 
+    LEFT JOIN (SELECT * FROM `tb_local` WHERE `tb_local`.`emp_Principal` = $emp_principal) AS L ON L.`extra_cod` = EE.`local` 
+    LEFT JOIN (SELECT * FROM `tb_modelo_equipamento` WHERE `tb_modelo_equipamento`.`emp_Principal` = $emp_principal) AS M ON M.`extra_cod` = EE.`modelo_equipamento` 
+    LEFT JOIN (SELECT * FROM `tb_subempresa` WHERE `tb_subempresa`.`emp_Principal` = $emp_principal) AS S ON S.`extra_cod` = EE.`empresa` 
+    LEFT JOIN (SELECT * FROM `tb_tipo_equipamento` WHERE `tb_tipo_equipamento`.`emp_Principal` = $emp_principal) AS T ON T.`extra_cod` = EE.`tipo_equipamento` 
+    WHERE EE.`emp_Principal` = $emp_principal AND EE.`extra_cod` = $codigo";
+
+        $statement = $conexao->prepare($query_tb_equipamento);
+
+        $statement->execute();
+
+        $result_tb_equipamento = $statement->fetchall();
+    ?>
+
+        <div class="container">
+            <div class="row">
+                <?php foreach ($result_tb_equipamento as $line) {
+                    if ($line['nota_fiscal'] == '') {
+                        $line['nota_fiscal'] = 'Não Informado';
+                    };
+                    if ($line['informacoes'] == '') {
+                        $line['informacoes'] = 'Não Informado';
+                    };
+                    if ($line['perifericos'] == '') {
+                        $line['perifericos'] = 'Nenhum vinculo';
+                    };
+                ?>
+                    <?php include 'conn_db.php'; ?>
+                    <section class="col-lg-4 ">
+                        <div>
+                            <span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Código</span>
+
+                            <span class="form-control" style="color: black!important; height: auto;"><?php echo $line['extra_cod'] ?></span>
+                        </div>
+                        <div>
+                            <span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Tipo</span>
+
+                            <select name="equipamento" class="form-control" id="equipamento">
+                            
+                                    <?php foreach ($result_tb_tipo_equipamento as $line1) { ?>
+                                        <option value="<?php echo $line1[1] ?>"><?php echo $line1[2] ?></option>
+                                <?php
+                                } ?>
+                            </select>
+                        </div>
+                        <div>
+                            <span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Modelo</span>
+
+                            <select name="modelo" class="form-control" id="modelo">
+
+                                <?php foreach ($result_tb_modelo_equipamento as $line1) { ?>
+                                    <option value="<?php echo $line1[1] ?>"><?php echo $line1[2] ?></option>
+                                <?php } ?>
+                            </select>
+                        </div>
+                        <div>
+                            <span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Fabricane</span>
+                            <select name="fabricante" class="form-control" id="fabricante">
+                                <?php foreach ($result_tb_fabricante as $line1) { ?>
+                                    <option value="<?php echo $line1[1] ?>"><?php echo $line1[2] ?></option>
+                                <?php } ?>
+                            </select>
+                        </div>
+                        <div>
+                            <span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Série</span>
+
+                            <span class="form-control" style="color: black!important; height: auto;"><?php echo $line['num_serie'] ?></span>
+                        </div>
+
+                    </section>
+
+                    <section class="col-lg-4 ">
+                        <div>
+                            <span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Empresa</span>
+                            <select name="empresa" class="form-control" id="empresa">
+
+                                <?php foreach ($result_tb_subEmp as $line1) { ?>
+                                    <option value="<?php echo $line1[1] ?>"><?php echo $line1[2] ?></option>
+                                <?php                                 } ?>
+                            </select>
+                        </div>
+                        <div>
+                            <span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Local</span>
+
+                            <select name="local" class="form-control" id="local">
+                        
+                                    <?php foreach ($result_tb_local as $line1) { ?>
+                                        <option value="<?php echo $line1[1] ?>"><?php echo $line1[2] ?></option>
+                                <?php } ?>
+                            </select>
+                        </div>
+                        <div>
+                            <span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Forncedor</span>
+
+                            <select name="fornecedor" class="form-control" id="fornecedor">
+                        
+                                    <?php foreach ($result_tb_fornecedor as $line1) { ?>
+                                        <option value="<?php echo $line1[1] ?>"><?php echo $line1[2] ?></option>
+                                <?php 
+                                } ?>
+                            </select>
+                        </div>
+                        <div>
+                            <span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Nota Fiscal</span>
+
+                            <span class="form-control" style="color: black!important; height: auto;"><?php echo $line['nota_fiscal'] ?></span>
+                        </div>
+                        <div>
+                            <span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Data da Compra</span>
+
+                            <span class="form-control" style="color: black!important; height: auto;"><?php echo $line['data_compra'] ?></span>
+                        </div>
+                    </section>
+                    <section class="col-lg-4 ">
+                        <div>
+                            <span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Situação</span>
+
+                            <select name="situacao" class="form-control" id="situacao">
+                                <option value="Operacional">Operacional</option>
+                                <option value="Sem_Uso">Sem Uso</option>
+                                <option value="Sucateado">Sucateado</option>
+                            </select>
                         </div>
                         <div>
                             <span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Informacões</span>
--- obj/gestao-app.php
+++ obj/gestao-app.php
@@ -51,119 +51,139 @@
 		});
 
 	}
+
+	function altera() {
+		id = document.getElementById('codigo').innerHTML;
+		//alert(id);
+		$(document).ready(function() {
+
+			$.ajax({
+				url: "../conn-db/conn_extra_gestao.php",
+				method: "POST",
+				data: {
+					verifica: 'altera',
+					cod: id
+				},
+				success: function(data) {
+					document.getElementById("result").innerHTML = data;
+
+				}
+			});
+
+		});
+	}
 </script>
 
 <body>
-<?php include '../conn-db/conn_pages.php'; ?>
-<?php if ($num_paginas == 0) { ?>
+	<?php include '../conn-db/conn_pages.php'; ?>
+	<?php if ($num_paginas == 0) { ?>
 
-<div class="container">
-<div style="margin: 10% 5%;">
+		<div class="container">
+			<div style="margin: 10% 5%;">
 				<p class="display-1">Inventário Vazio</p>
 				<p class="h1">Volte após realizar o primeiro cadastro</p>
 			</div>
-</div>
-<?php  } else if ($num_paginas > 0) { ?>
-	<section>
-	</section>
-	<section>
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-6 ">
+		</div>
+	<?php  } else if ($num_paginas > 0) { ?>
+		<section>
+		</section>
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6 ">
 
-					<input class="btn btn-lg btn-block" type="submit" value="Gestão de Equipamento">
+						<input class="btn btn-lg btn-block" type="submit" value="Gestão de Equipamento">
 
-				</div>
-				<div class="col-sm-6 ">
+					</div>
+					<div class="col-sm-6 ">
 
-					<input class="btn btn-lg btn-block" type="submit" value="Gestão de Itens">
+						<input class="btn btn-lg btn-block" type="submit" value="Gestão de Itens">
 
-				</div>
+					</div>
 
 
-				<div class="col-sm-12 ">
+					<div class="col-sm-12 ">
 
-					<!-- Principal -->
-					<section>
+						<!-- Principal -->
+						<section>
 
-						<div class="row" id="userscroll" style="overflow: auto; text-align: center">
-							<?php include '../conn-db/conn_db.php';
-							/*echo '<pre>';
+							<div class="row" id="userscroll" style="overflow: auto; text-align: center">
+								<?php include '../conn-db/conn_db.php';
+								/*echo '<pre>';
 							print_r($result_tb_equipamento);
 							echo '</pre>';*/ ?>
-							<?php foreach ($result_tb_equipamento as $line) {
-								if ($line['nota_fiscal'] == '') {
-									$line['nota_fiscal'] = 'Não Informado';
-								};
-								if ($line['informacoes'] == '') {
-									$line['informacoes'] = 'Não Informado';
-								};
-								if ($line['perifericos'] == '') {
-									$line['perifericos'] = 'Nenhum vinculo';
-								};
-							?>
-								<div class="col-md-4 border_smaler" style="text-align: -webkit-center;">
-									<div>
-										<img src="../img/icons_devices.png" alt="icone">
-									</div>
-									<div>
-										<div class="input-group" style="display: block!important;">
-											<div>
-												<span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Código</span>
+								<?php foreach ($result_tb_equipamento as $line) {
+									if ($line['nota_fiscal'] == '') {
+										$line['nota_fiscal'] = 'Não Informado';
+									};
+									if ($line['informacoes'] == '') {
+										$line['informacoes'] = 'Não Informado';
+									};
+									if ($line['perifericos'] == '') {
+										$line['perifericos'] = 'Nenhum vinculo';
+									};
+								?>
+									<div class="col-md-4 border_smaler" style="text-align: -webkit-center;">
+										<div>
+											<img src="../img/icons_devices.png" alt="icone">
+										</div>
+										<div>
+											<div class="input-group" style="display: block!important;">
+												<div>
+													<span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Código</span>
 
-												<span class="form-control" style="color: black!important; height: auto;"><?php echo $line['extra_cod'] ?></span>
-											</div>
-											<div>
-												<span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Tipo</span>
+													<span class="form-control" style="color: black!important; height: auto;"><?php echo $line['extra_cod'] ?></span>
+												</div>
+												<div>
+													<span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Tipo</span>
 
-												<span class="form-control" style="color: black!important; height: auto;"><?php echo $line['tipo_equipamento'] ?></span>
-											</div>
-											<div>
-												<span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Modelo</span>
+													<span class="form-control" style="color: black!important; height: auto;"><?php echo $line['tipo_equipamento'] ?></span>
+												</div>
+												<div>
+													<span class="input-group-text" style="color: black!important; background-color: #e9ecef !important; border: 1px solid #d4dadf !important;">Modelo</span>
 
-												<span class="form-control" style="color: black!important; height: auto;"><?php echo $line['modelo_equipamento'] ?></span>
+													<span class="form-control" style="color: black!important; height: auto;"><?php echo $line['modelo_equipamento'] ?></span>
+												</div>
 											</div>
 										</div>
-									</div>
 
-									<div style="padding: 10px 0;">
-										<img class="usr_btn" src="../img/plus.png" alt="Sinal_Mais" data-toggle="modal" data-target="#exampleModalScrollable" onclick="extras('<?php echo $line['extra_cod'] ?>')">
-										<img class="usr_btn" src="../img/trash.png" alt="lixeira" data-toggle="modal" data-target="#exampleModalCenter" onclick="">
+										<div style="padding: 10px 0;">
+											<img class="usr_btn" src="../img/plus.png" alt="Sinal_Mais" data-toggle="modal" data-target="#exampleModalScrollable" onclick="extras('<?php echo $line['extra_cod'] ?>')">
+											<img class="usr_btn" src="../img/trash.png" alt="lixeira">
+										</div>
 									</div>
-								</div>
-							<?php } ?>
-						</div>
+								<?php } ?>
+							</div>
 
-					</section>
-					<!-- Principal -->
-					<!-- Modal Principal -->
-					<section>
+						</section>
+						<!-- Principal -->
+						<!-- Modal Principal -->
+						<section>
 
-						<div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
-							<div class="modal-dialog modal-lg modal-dialog-scrollable" role="document">
-								<div class="modal-content">
-									<div class="modal-header">
-										<h5 class="modal-title" id="exampleModalScrollableTitle">Equipamento</h5>
-										<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-											<span aria-hidden="true">&times;</span>
-										</button>
-									</div>
-									<div class="modal-body">
-										<div id="result"></div>
-									</div>
-									<div class="modal-footer">
-										<button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+							<div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+								<div class="modal-dialog modal-lg modal-dialog-scrollable" role="document">
+									<div class="modal-content">
+										<div class="modal-header">
+											<h5 class="modal-title" id="exampleModalScrollableTitle">Equipamento</h5>
+											
+										</div>
+										<div class="modal-body">
+											<div id="result"></div>
+										</div>
+										<div class="modal-footer">
+											<button type="button" class="btn btn-secondary" onclick="altera()">Alterar</button>
+											<button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+										</div>
 									</div>
 								</div>
 							</div>
-						</div>
-					</section>
-					<!-- Fim Modal Principal -->
+						</section>
+						<!-- Fim Modal Principal -->
+					</div>
 				</div>
 			</div>
-		</div>
-		</div>
-	</section>
+			</div>
+		</section>
 	<?php } ?>
 </body>
 
